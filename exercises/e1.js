82cb4e67cdeb3c71a54abf5a46aa4be1c8612e4d
Boss Man <joshualicht18@gmail.com>
2023-02-22 13:22:33 -0600

    completed e1

--- exercises/e1.js
+++ exercises/e1.js
@@ -13,6 +13,9 @@
 
 // Your code goes here
 
+function logMessage() {
+  console.log('Hello, World!');
+}
 
 
 /**
@@ -27,7 +30,9 @@
 
 // Your code goes here...
 
-
+function logMyMessage(message) {
+  console.log(message);
+}
 
 /**
  * ===============================================================
@@ -40,7 +45,9 @@
 
 // Your code goes here...
 
-
+function logMessageWithMyName(name) {
+  console.log(`My name is ${name}!`);
+}
 
 /**
  * ==============================================================
@@ -54,7 +61,9 @@
 
 // Your code goes here...
 
-
+function logMyInfo(name, age) {
+  console.log(`My name is ${name}. I am ${age} years old!`);
+}
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-1"
