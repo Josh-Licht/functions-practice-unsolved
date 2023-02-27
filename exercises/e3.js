e78dcb99f0faeb1e46087a5c69e800bada898218
Boss Man <joshualicht18@gmail.com>
2023-02-27 09:55:34 -0600

    improved e3 functions

--- exercises/e3.js
+++ exercises/e3.js
@@ -46,27 +46,24 @@ function doesArrayIncludeItemsBetweenVals(arr, val1, val2) {
 function getValueWithConditionOne(num1, num2) {
   if (num1 === 40 && num2 === 40) {
     return num1 + num2;
-  } else {
-    return (num1 + num2) * 2;
-  }
+  } 
+  return (num1 + num2) * 2;
 }
 
 // Arrow function
 const getValueWithConditionTwo = (num1, num2) => {
   if (num1 === 40 && num2 === 40) {
     return num1 + num2;
-  } else {
-    return (num1 + num2) * 2;
   }
+  return (num1 + num2) * 2;
 };
 
 // Function expression with anonymous function
 const getValueWithConditionThree = function(num1, num2) {
   if (num1 === 40 && num2 === 40) {
     return num1 + num2;
-  } else {
-    return (num1 + num2) * 2;
   }
+  return (num1 + num2) * 2;
 };
 
 // === TEST YOURSELF ===
